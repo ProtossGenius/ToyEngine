5a1bbfd87024603e12bddb141c605c8229e85bf5
FakeName <protossgenius@gmail.com>
2025-02-18 15:41:47 +0800

    fix a init list npe error.

--- engine/src/main/java/com/suremoon/game/kernel/initer/InitListItf.java
+++ engine/src/main/java/com/suremoon/game/kernel/initer/InitListItf.java
@@ -1,34 +1,37 @@
 package com.suremoon.game.kernel.initer;
 
 import com.suremoon.game.door.tools.IDManager;
+
 import java.io.File;
 
 public interface InitListItf {
-  void init(Progress progress) throws Exception;
+    void init(Progress progress) throws Exception;
 
-  default void loadList(String path, String cfgSuffix, NameInit nameInit, Progress progress)
-      throws Exception {
-    File f = new File(path);
-    File[] effectCfgs = f.listFiles();
-    assert effectCfgs != null;
-    progress.setTotal(effectCfgs.length);
-    for (File effectCfg : effectCfgs) {
-      try {
-        String name = effectCfg.getName();
-        if (!name.endsWith(cfgSuffix)) {
-          continue;
+    default void loadList(String path, String cfgSuffix, NameInit nameInit, Progress progress)
+            throws Exception {
+        File f = new File(path);
+        File[] effectCfgs = f.listFiles();
+        if (effectCfgs == null) {
+            return;
         }
+        progress.setTotal(effectCfgs.length);
+        for (File effectCfg : effectCfgs) {
+            try {
+                String name = effectCfg.getName();
+                if (!name.endsWith(cfgSuffix)) {
+                    continue;
+                }
 
-        name = name.substring(0, name.length() - cfgSuffix.length());
-        IDManager.getID(name);
-        nameInit.Init(name);
-      } finally {
-        progress.done();
-      }
+                name = name.substring(0, name.length() - cfgSuffix.length());
+                IDManager.getID(name);
+                nameInit.Init(name);
+            } finally {
+                progress.done();
+            }
+        }
     }
-  }
 
-  interface NameInit {
-    void Init(String name) throws Exception;
-  }
+    interface NameInit {
+        void Init(String name) throws Exception;
+    }
 }
